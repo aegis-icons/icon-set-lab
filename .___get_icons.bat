03b9734a1fcd00ac319139ea0d42ce2e2a10f85c
Kristian Salonen <github@krisu.anonaddy.com>
2023-03-18 23:09:39 +0200

    get_icons.bat: check if either "aegis-icons" or "aegis-icons-master" dir exists

--- .___get_icons.bat
+++ .___get_icons.bat
@@ -1,5 +1,20 @@
+@echo off
 set website-root=%cd%
 cd ..
-cd aegis-icons
+if exist aegis-icons\ (
+  cd aegis-icons
+  goto copy_icons
+  ) else (
+    if exist aegis-icons-master\ (
+    cd aegis-icons-master
+    goto copy_icons
+    ) else (
+      echo Please include "aegis-icons" or "aegis-icons-master" folder!
+      echo.
+      pause
+      exit
+    )
+)
+:copy_icons
 robocopy "%cd%\_templates" "%website-root%" z_icon_grid.svg
 robocopy "%cd%\icons" "%website-root%\icons" /mir
