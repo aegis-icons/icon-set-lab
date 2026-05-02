b985eb411382f7be5194df3d7d6ad85a6e4a9278
Kristian Salonen <3540275+krisu5@users.noreply.github.com>
2026-05-02 19:49:06 +0300

    Minor sh/bat tweak

--- z__bat-scripts/2__start_jekyll_server.bat
+++ z__bat-scripts/2__start_jekyll_server.bat
@@ -1,7 +1,7 @@
 @echo off
 title "Local Jekyll server for icon-set-lab"
 cd ..
-echo [41mDon't close the window, use CTRL+C instead! Otherwise Ruby stays running in the background![0m
+echo [41m Don't close the window, use CTRL+C instead! Otherwise Ruby stays running in the background! [0m
 echo.
 bundle exec jekyll serve --host localhost --open_url
 pause
